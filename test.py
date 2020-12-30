c865cc14fb6d5a4ff2e13f025b00483a5b41e8d2
André Kuljis <65884095+lilkimo@users.noreply.github.com>
2020-12-30 20:57:59 -0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-def pascalNivel(nivel):
-    coeficiente = 1
-    numeros = []
-    for iterador in range(1, nivel + 1):  
-        numeros.append(coeficiente)
-        coeficiente = int(coeficiente * (nivel - iterador) / iterador)
-    return numeros
-
-print(pascalNivel(11))
